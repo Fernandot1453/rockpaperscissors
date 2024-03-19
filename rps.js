78e2fa0b0c78fa4ae7f21b4f5273418d7fdc7136
fernandot1453 <fernandot1453@gmail.com>
2024-03-19 14:50:12 -0700

    created computerselect function

---
+++ rps.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let choice = Math.random() 
+        if (choice <= 0.33) {
+            choice = "rock"
+        } else if (choice <= 0.66) {
+            choice = "scissors"
+        } else {
+            choice = "paper"
+        }
+        return choice
+}
+
+console.log(getComputerChoice())
+
+function game(player, computer) {
+    if (player = 'rock'||'')
+    console.log()
+}
